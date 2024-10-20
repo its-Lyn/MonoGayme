a90e57dc40f26af1901d954c06aa0cbf86bc2112
its-Lyn <143884194+its-Lyn@users.noreply.github.com>
2024-10-20 22:12:08 +0300

    Add GetFirstButton.

--- MonoGayme/Utilities/InputManager.cs
+++ MonoGayme/Utilities/InputManager.cs
@@ -63,6 +63,42 @@ public static class InputManager
     public static bool IsGamePadPressed(Buttons btn) => _currentControllerState.IsButtonDown(btn) && _previousControllerState.IsButtonUp(btn);
 
     /// <summary>
+    /// Get first pressed button on the gamepad.
+    /// </summary>
+    public static Buttons? GetFirstButton()
+    {
+        // WHat the fuck
+        if (_currentControllerState.Buttons.A == ButtonState.Pressed) return Buttons.A;
+        if (_currentControllerState.Buttons.B == ButtonState.Pressed) return Buttons.B;
+        if (_currentControllerState.Buttons.X == ButtonState.Pressed) return Buttons.X;
+        if (_currentControllerState.Buttons.Y == ButtonState.Pressed) return Buttons.Y;
+        if (_currentControllerState.Buttons.Start == ButtonState.Pressed) return Buttons.Start;
+        if (_currentControllerState.Buttons.Back == ButtonState.Pressed) return Buttons.Back;
+        if (_currentControllerState.Buttons.LeftShoulder == ButtonState.Pressed) return Buttons.LeftShoulder;
+        if (_currentControllerState.Buttons.RightShoulder == ButtonState.Pressed) return Buttons.RightShoulder;
+        if (_currentControllerState.Buttons.LeftStick == ButtonState.Pressed) return Buttons.LeftStick;
+        if (_currentControllerState.Buttons.RightStick == ButtonState.Pressed) return Buttons.RightStick;
+
+        if (_currentControllerState.DPad.Up == ButtonState.Pressed) return Buttons.DPadUp;
+        if (_currentControllerState.DPad.Down == ButtonState.Pressed) return Buttons.DPadDown;
+        if (_currentControllerState.DPad.Left == ButtonState.Pressed) return Buttons.DPadLeft;
+        if (_currentControllerState.DPad.Right == ButtonState.Pressed) return Buttons.DPadRight;
+        
+        if (_currentControllerState.Triggers.Left > 0.5f) return Buttons.LeftTrigger;
+        if (_currentControllerState.Triggers.Right > 0.5f) return Buttons.RightTrigger;
+        if (_currentControllerState.ThumbSticks.Left.X < -0.5f) return Buttons.LeftThumbstickLeft;
+        if (_currentControllerState.ThumbSticks.Left.X > 0.5f) return Buttons.LeftThumbstickRight;
+        if (_currentControllerState.ThumbSticks.Left.Y < -0.5f) return Buttons.LeftThumbstickDown;
+        if (_currentControllerState.ThumbSticks.Left.Y > 0.5f) return Buttons.LeftThumbstickUp;
+        if (_currentControllerState.ThumbSticks.Right.X < -0.5f) return Buttons.RightThumbstickLeft;
+        if (_currentControllerState.ThumbSticks.Right.X > 0.5f) return Buttons.RightThumbstickRight;
+        if (_currentControllerState.ThumbSticks.Right.Y < -0.5f) return Buttons.RightThumbstickDown;
+        if (_currentControllerState.ThumbSticks.Right.Y > 0.5f) return Buttons.RightThumbstickUp;
+        
+        return null;
+    }
+    
+    /// <summary>
     /// Update the input device state. Must only be run once a frame.
     /// </summary>
     public static void GetState()
